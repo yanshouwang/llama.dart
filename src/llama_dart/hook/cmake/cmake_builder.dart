4ed89311971210bfaab44a9ea0ec7172fe506231
yanshouwang <yanshouwang@zeekr.dev>
2026-04-07 14:44:54 +0800

    调整build_hooks

--- src/llama_dart/hook/cmake/cmake_builder.dart
+++ src/llama_dart/hook/cmake/cmake_builder.dart
@@ -37,16 +37,18 @@ final class CMakeBuilder implements Builder {
       );
       return;
     }
+
     final git = await Tools.git.resolve(logger: logger);
     final cmake = await Tools.cmake.resolve(logger: logger);
 
-    final srcDir = input.outputDirectory.resolve('src/');
+    final srcDir = input.outputDirectoryShared.resolve('src/');
     final dstDir = input.outputDirectory.resolve('dst/');
     final outDir = input.outputDirectory.resolve('out/');
     await Directory.fromUri(srcDir).create(recursive: true);
     await Directory.fromUri(dstDir).create(recursive: true);
     await Directory.fromUri(outDir).create(recursive: true);
 
+    // git rev-parse --show-toplevel
     final topLevel = await runProcess(
       executable: git.uri,
       arguments: ['rev-parse', '--show-toplevel'],
@@ -54,7 +56,7 @@ final class CMakeBuilder implements Builder {
       logger: logger,
       throwOnUnexpectedExitCode: false,
     ).then((e) => e.stdout.trim());
-    logger.info('topLevel: $topLevel');
+    // git clone
     if (Uri.directory(topLevel) != srcDir) {
       await runProcess(
         executable: git.uri,
@@ -64,6 +66,7 @@ final class CMakeBuilder implements Builder {
         throwOnUnexpectedExitCode: true,
       );
     }
+    // git checkout
     await runProcess(
       executable: git.uri,
       arguments: ['checkout', tag],
@@ -71,24 +74,38 @@ final class CMakeBuilder implements Builder {
       logger: logger,
       throwOnUnexpectedExitCode: true,
     );
+    // cmake -B
     await runProcess(
       executable: cmake.uri,
       arguments: [
         '-B',
         dstDir.toFilePath(),
         '-DCMAKE_PLATFORM_NO_VERSIONED_SONAME=ON',
+        '-DCMAKE_BUILD_TYPE=Release',
+        ..._getTargetArguments(input.config.code),
+        '-DLLAMA_BUILD_COMMON=OFF',
+        '-DLLAMA_BUILD_TESTS=OFF',
+        '-DLLAMA_BUILD_TOOLS=OFF',
+        '-DLLAMA_BUILD_EXAMPLES=OFF',
+        '-DLLAMA_BUILD_SERVER=OFF',
+        '-DLLAMA_BUILD_WEBUI=OFF',
+        '-DLLAMA_TOOLS_INSTALL=OFF',
+        '-DLLAMA_TESTS_INSTALL=OFF',
+        '-DGGML_NATIVE=OFF',
       ],
       workingDirectory: srcDir,
       logger: logger,
       throwOnUnexpectedExitCode: true,
     );
+    // cmake --build
     await runProcess(
       executable: cmake.uri,
-      arguments: ['--build', dstDir.toFilePath(), '--config', 'Release'],
+      arguments: ['--build', dstDir.toFilePath()],
       workingDirectory: srcDir,
       logger: logger,
       throwOnUnexpectedExitCode: true,
     );
+    // cmake --install
     await runProcess(
       executable: cmake.uri,
       arguments: [
@@ -102,8 +119,8 @@ final class CMakeBuilder implements Builder {
       throwOnUnexpectedExitCode: true,
     );
 
-    final linkMode = getLinkMode(input.config.code.linkModePreference);
     final libDir = outDir.resolve('lib/');
+    final linkMode = getLinkMode(input.config.code.linkModePreference);
     final libName = input.config.code.targetOS.libraryFileName(name, linkMode);
     final libExt = path.extension(libName);
     final libAsset = CodeAsset(
@@ -131,4 +148,54 @@ final class CMakeBuilder implements Builder {
       output.assets.code.add(fileAsset);
     }
   }
+
+  List<String> _getTargetArguments(
+    CodeConfig config,
+  ) => switch (config.targetOS) {
+    .macOS => [
+      '-DCMAKE_OSX_ARCHITECTURES=${config.targetArchitecture.toOsxArchitecture()}',
+      // '-DCMAKE_OSX_DEPLOYMENT_TARGET=${config.macOS.targetVersion}',
+      '-DCMAKE_OSX_DEPLOYMENT_TARGET=13.3',
+    ],
+    .iOS => [
+      '-DCMAKE_SYSTEM_NAME=iOS',
+      '-DCMAKE_OSX_ARCHITECTURES=${config.targetArchitecture.toOsxArchitecture()}',
+      '-DCMAKE_OSX_SYSROOT=${config.iOS.targetSdk.type}',
+      '-DCMAKE_OSX_DEPLOYMENT_TARGET=${config.iOS.targetVersion}',
+    ],
+    .android => [
+      '-DCMAKE_SYSTEM_NAME=Android',
+      '-DANDROID_ABI=${config.targetArchitecture.toAndroidAbi()}',
+    ],
+    _ => [
+      '-DCMAKE_SYSTEM_PROCESSOR=${config.targetArchitecture.toSystemProcessor()}',
+    ],
+  };
+}
+
+extension on Architecture {
+  String toOsxArchitecture() => switch (this) {
+    .arm64 => 'arm64',
+    .x64 => 'x86_64',
+    _ => throw UnsupportedError('Unsupported architecture: $this'),
+  };
+
+  String toAndroidAbi() => switch (this) {
+    .arm => 'armeabi-v7a',
+    .arm64 => 'arm64-v8a',
+    .ia32 => 'x86',
+    .x64 => 'x86_64',
+    .riscv64 => 'riscv64',
+    _ => throw UnsupportedError('Unsupported architecture: $this'),
+  };
+
+  String toSystemProcessor() => switch (this) {
+    .arm => 'arm',
+    .arm64 => 'arm64',
+    .ia32 => 'x86',
+    .riscv32 => 'riscv32',
+    .riscv64 => 'riscv64',
+    .x64 => 'x86_64',
+    _ => throw UnsupportedError('Unsupported architecture: $this'),
+  };
 }
--- src/llama_flutter/macos/Classes/llama_flutter.c
+++
@@ -1,3 +0,0 @@
-// Relative import to be able to reuse the C sources.
-// See the comment in ../llama_flutter.podspec for more information.
-#include "../../src/llama_flutter.c"
